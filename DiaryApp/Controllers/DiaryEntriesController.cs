bacabede8009400a9b8b19ff79e201ff93d2e050
Mikołaj <pzx112240@student.poznan.merito.pl>
2024-09-17 08:32:19 +0200

    Added a Save to database option(full-working)

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -13,15 +13,25 @@ public class DiaryEntriesController : Controller
         _db = db;
     }
 
-    //public IActionResult Add()
-    //{
-    //    return View();
-    //}
-
     public IActionResult Index()
     {
         List<DiaryEntry> entries = _db.DiaryEntries.ToList();
 
         return View(entries);
+    }
+
+    public IActionResult Create()
+    {
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(DiaryEntry obj)
+    {
+        obj.Created = obj.Created.ToUniversalTime(); // Convert to UTC
+        _db.DiaryEntries.Add(obj);//adds the new diary entry to the database
+        _db.SaveChanges();//saves the changes to the database
+        return RedirectToAction("Index", "Home");
     }
 }
